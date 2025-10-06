1a0fb5d338d22b17269b9fd7f71cc9d4ea4925d1
siriuss <shreyash@siriusss-MacBook-Pro.local>
2025-10-06 22:29:09 +0530

    download-bat

--- download.bat
+++ download.bat
@@ -2,5 +2,5 @@
 REM Change directory to the script location
 cd /d "%~dp0"
 REM Run PowerShell script with temporary bypass
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "dl.ps1" %*
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "dld.ps1" %*
 pause
